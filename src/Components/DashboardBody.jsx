4fb69b2e89cae46ad53e66f40ffa9d8e4e787985
survi shirodkar <survi@bluestream.io>
2021-04-13 21:48:25 +0530

    new files

---
+++ src/Components/DashboardBody.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function DashboardBody() {
+    return (
+        <div>
+           <h3>hello survi</h3> 
+        </div>
+    )
+}
---
+++ src/constant.jsx
@@ -0,0 +1,30 @@
+
+
+export let constantarray={
+        users :[{firstname:"survi",
+                lastname:"shirodkar",
+                contact:"7768076298",
+                gender:"female",
+                email:"survishirodkar@gmail.com",
+                eventname:"fact",
+                password:"shailesh"},
+     
+                {firstname:"urvi",
+                lastname:"shirodkar",
+                contact:"9370071402",
+                gender:"female",
+                email:"urvishirodkar@gmail.com",
+                eventname:"fact1",
+                password:"shailesh22"},
+     
+                {firstname:"vi",
+                lastname:"shirodkar",
+                contact:"7768076299",
+                gender:"female",
+                email:"vishirodkar@gmail.com",
+                eventname:"fact2",
+                password:"shailesh16"}
+     
+             ]
+     }
+   
